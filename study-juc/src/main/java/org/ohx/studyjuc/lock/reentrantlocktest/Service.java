45ae7c6beae5ab36e0428afb93b324fc7a176538
ohx <mupkid123456@gmail.com>
2022-06-26 21:34:53 +0800

    JUC学习：Lock使用

---
+++ study-juc/src/main/java/org/ohx/studyjuc/lock/reentrantlocktest/Service.java
@@ -0,0 +1,23 @@
+package org.ohx.studyjuc.lock.reentrantlocktest;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author mudkip
+ * @date 2022/6/26
+ */
+public class Service {
+    private final Lock lock = new ReentrantLock();
+
+    public void testMethod() {
+        try {
+            lock.lock();
+            for (int i = 0; i < 5; i++) {
+                System.out.println("ThreadName=" + Thread.currentThread().getName() + " " + i);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+}
