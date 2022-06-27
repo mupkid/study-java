8f0085074c720746a1f29f1de65e75c139d78eea
ohx <mupkid123456@gmail.com>
2022-06-27 16:30:53 +0800

    JUC学习：Lock使用

---
+++ study-juc/src/main/java/org/ohx/studyjuc/lock/readwritelock/WriteWrite.java
@@ -0,0 +1,17 @@
+package org.ohx.studyjuc.lock.readwritelock;
+
+/**
+ * @author mudkip
+ * @date 2022/6/27
+ */
+public class WriteWrite {
+    public static void main(String[] args) {
+        Service service = new Service();
+        new Thread(() -> {
+            service.write();
+        }).start();
+        new Thread(() -> {
+            service.write();
+        }).start();
+    }
+}
