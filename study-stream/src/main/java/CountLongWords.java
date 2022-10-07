cd13b5ea0425f9378a3e795a704153f9a5b76eea
ohx <mupkid123456@gmail.com>
2022-10-07 22:06:49 +0800

    Stream API 学习

--- study-stream/src/main/java/CountLongWords.java
+++ study-stream/src/main/java/CountLongWords.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CountLongWords {
-
     public static void main(String[] args) throws IOException {
         String contents = new String(Files.readAllBytes(Paths.get("file/aaa.txt")), StandardCharsets.UTF_8);
         // Split into words: nonletters are delimiters
@@ -14,8 +13,8 @@ public class CountLongWords {
 
         System.out.println(System.currentTimeMillis());
         long count = 0;
-        for(String w : words){
-            if(w.length() >12){
+        for (String w : words) {
+            if (w.length() > 12) {
                 count++;
             }
         }
@@ -23,12 +22,12 @@ public class CountLongWords {
 
         System.out.println(System.currentTimeMillis());
 
-        count = words.stream().filter(w -> w.length()>12).count();
+        count = words.stream().filter(w -> w.length() > 12).count();
         System.out.println(count);
 
         System.out.println(System.currentTimeMillis());
 
-        count = words.parallelStream().filter(w -> w.length()>12).count();
+        count = words.parallelStream().filter(w -> w.length() > 12).count();
         System.out.println(count);
 
         System.out.println(System.currentTimeMillis());
--- study-stream/src/main/java/CreatingStreams.java
+++ study-stream/src/main/java/CreatingStreams.java
@@ -1,11 +1,42 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * @author mi
+ */
 public class CreatingStreams {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("file/aaa.txt");
+        String contents = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
 
-    public static <T> void show(String title, Stream<T> stream){
-        final int SIZE =10;
-        List<T> firstElements = stream.limit(SIZE+1).collect(Collectors.toList());
+        Stream<String> words = Stream.of(contents.split("\\PL+"));
+        show("words", words);
+        Stream<String> song = Stream.of("gently", "down", "the", "stream");
+        show("song", song);
+        Stream<Object> silence = Stream.empty();
+        show("silence", silence);
+    }
+
+    public static <T> void show(String title, Stream<T> stream) {
+        final int SIZE = 10;
+        List<T> firstElements = stream.limit(SIZE + 1).collect(Collectors.toList());
+        System.out.println(title + ": ");
+        for (int i = 0; i < firstElements.size(); i++) {
+            if (i > 0) {
+                System.out.println(", ");
+            }
+            if (i < SIZE) {
+                System.out.println(firstElements.get(i));
+            } else {
+                System.out.println("...");
+            }
+        }
+        System.out.println();
     }
 }
